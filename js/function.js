76d56c28498132d2bdf83d75e48f5d375a02aa5f
Evgeny Ivanov <busforward@gmail.com>
2016-11-26 17:43:55 +0500

    First commit

---
+++ js/function.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+
+	// First screen full height
+	function setHeiHeight() {
+	    $('.full__height').css({
+	        minHeight: $(window).height() + 'px'
+	    });
+	}
+	setHeiHeight(); // устанавливаем высоту окна при первой загрузке страницы
+	$(window).resize( setHeiHeight ); // обновляем при изменении размеров окна
+
+
+	// Reset link whte attribute href="#"
+	$('[href*="#"]').click(function(event) {
+		event.preventDefault();
+	});
+
+	// Scroll to ID // Плавный скролл к элементу при нажатии на ссылку. В ссылке указываем ID элемента
+	// $('#main__menu a[href^="#"]').click( function(){ 
+	// 	var scroll_el = $(this).attr('href'); 
+	// 	if ($(scroll_el).length != 0) {
+	// 	$('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500);
+	// 	}
+	// 	return false;
+	// });
+
+	// Stiky menu // Липкое меню. При прокрутке к элементу #header добавляется класс .stiky который и стилизуем
+    // $(document).ready(function(){
+    //     var HeaderTop = $('#header').offset().top;
+        
+    //     $(window).scroll(function(){
+    //             if( $(window).scrollTop() > HeaderTop ) {
+    //                     $('#header').addClass('stiky');
+    //             } else {
+    //                     $('#header').removeClass('stiky');
+    //             }
+    //     });
+    // });
+
+    setTimeout(function() {
+	    $('.next_section').addClass('animated bounce');
+    }, 4000);
+
+    $('.slick').slick({
+    	arrows: false,
+		dots: true,
+		infinite: true,
+		speed: 0,
+		fade: true,
+		adaptiveHeight: true
+		// cssEase: 'linear'
+	});
+
+});
