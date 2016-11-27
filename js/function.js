470ae6497253e75bc499039498b4f1b8b42be802
Evgeny Ivanov <busforward@gmail.com>
2016-11-27 18:11:56 +0500

    Last change

--- js/function.js
+++ js/function.js
@@ -16,13 +16,13 @@ $(document).ready(function() {
 	});
 
 	// Scroll to ID // Плавный скролл к элементу при нажатии на ссылку. В ссылке указываем ID элемента
-	// $('#main__menu a[href^="#"]').click( function(){ 
-	// 	var scroll_el = $(this).attr('href'); 
-	// 	if ($(scroll_el).length != 0) {
-	// 	$('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500);
-	// 	}
-	// 	return false;
-	// });
+	$('a.next_section').click( function(){ 
+		var scroll_el = $(this).attr('href'); 
+		if ($(scroll_el).length != 0) {
+		$('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500);
+		}
+		return false;
+	});
 
 	// Stiky menu // Липкое меню. При прокрутке к элементу #header добавляется класс .stiky который и стилизуем
     // $(document).ready(function(){
@@ -47,8 +47,35 @@ $(document).ready(function() {
 		infinite: true,
 		speed: 0,
 		fade: true,
-		adaptiveHeight: true
+		adaptiveHeight: true,
+		responsive: [
+			{
+				breakpoint: 761,
+				settings: {
+					speed: 500,
+					fade: false,
+				}
+			}
+		]
 		// cssEase: 'linear'
+	});
+
+	$(".phone").mask("+7 (999) 999-9999");
+
+	$('#price .grid__wrapper,.partners_grid .grid__wrapper').viewportChecker({
+		classToAdd: 'animated fadeInUp',
+	});
+
+	$('.package_list').viewportChecker({
+		classToAdd: 'animated flipInX',
+	});
+
+	$('.how_work_item:first-child,.how_work_item:nth-child(3)').viewportChecker({
+		classToAdd: 'animated fadeInLeft',
+	});
+
+	$('.how_work_item:last-child,.how_work_item:nth-child(2)').viewportChecker({
+		classToAdd: 'animated fadeInRight',
 	});
 
 });
